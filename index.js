2453e0d6a567d023472e9c027a00548bdf7f13d2
TSUNG-YU WU <48641178+orps2678@users.noreply.github.com>
2023-10-02 14:36:09 +0800

    Vue基礎入門
    展示列表內容

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const app = Vue.createApp({
     data() {
         return {
-            name: {
-                firstName:"明"   
-            },
+            todos:[
+                {
+                    content:"把項目做完",
+                    complete:true,
+                },
+                {
+                    content:"去超市購物",
+                    complete:true ,
+                },
+                {
+                    content:"看10分鐘的書",
+                    complete:false, 
+                }
+            ]
         };
     },
 });
