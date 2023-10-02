982965c72445f6d1491d01d66bfa267b960243aa
TSUNG-YU WU <48641178+orps2678@users.noreply.github.com>
2023-10-02 14:52:30 +0800

    Vue基礎入門
    根據條件展示頁面內容
    1.v-if, v-else-if, v-else內容須相鄰使用
    2.v-show會設置display屬性

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
 const app = Vue.createApp({
     data() {
         return {
-            todos:[
-                {
-                    content:"把項目做完",
-                    complete:true,
-                },
-                {
-                    content:"去超市購物",
-                    complete:true ,
-                },
-                {
-                    content:"看10分鐘的書",
-                    complete:false, 
-                }
-            ]
+            books: []
         };
     },
 });
